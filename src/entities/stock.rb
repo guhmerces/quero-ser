ce0dc6b7c541252dc11023214c209c8652971f89
guhmerces <guhmerces@hotmail.com>
2023-04-20 03:03:43 -0300

    Add minimum transfer amount data to Stock

--- src/entities/stock.rb
+++ src/entities/stock.rb
@@ -1,13 +1,19 @@
 #todo : make code decoupled relating to the distribution center name
 module Entities
     class Stock
+        attr_reader :products, :transfer_min
     
         def initialize(products)
             @products = products
+            @transfer_min = 10
         end
     
         def self.call(products)
             new(products)
+        end
+
+        def should_transfer_min?(n)
+            0 < n && n < @transfer_min
         end
     end
 end
