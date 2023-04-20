f7b5f6bb970e65280031ecbdf0386645fda0b0fc
guhmerces <guhmerces@hotmail.com>
2023-04-20 03:10:46 -0300

    Add methods to Product

--- src/entities/product.rb
+++ src/entities/product.rb
@@ -11,5 +11,20 @@ module Entities
         def self.call(cod:, qty:, min_qty:, **_)
             new(cod, qty, min_qty)
         end
+
+        def reserve(amount:)
+            @qty -= amount
+        end
+
+        def replacement?
+            @min_qty > @qty
+        end
+
+        def replacement_amount
+            if not replacement?
+                return 0
+            end
+            @min_qty - @qty
+        end
     end    
 end
